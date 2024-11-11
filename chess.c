725bef80c36aaddd9d47f43b0c5f91a7c8c72002
Iv4n0f <ilopezz@unsa.edu.pe>
2024-11-10 21:49:38 -0500

    Tratamos de solucionar el problema pasando triple para poder desregistrar el puntero doble ya que necesitamos su direccion de memoria y originalmente esto no era accesible ya que el argumento de unlinkMemory() era local

--- chess.c
+++ chess.c
@@ -6,17 +6,17 @@
 char** allocateMemory(int rows, size_t cols){
   char** newFig;
   memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1));
-  //for(int i = 0; i < rows; i++)
-    //memoryAlloc((void**)&newFig[i], sizeof(char)*(cols + 1));
+  for(int i = 0; i < rows; i++)
+    memoryAlloc((void**)&newFig[i], sizeof(char)*(cols + 1));
   return newFig;
 }
 
-void unlinkMemory(char** fig){
+void unlinkMemory(char*** fig){
   countMemoryEntries();
-  unregisterPointer((void**)&fig);
+  for(int i = 0; fig[i]; i++)
+    unregisterPointer((void**)*(&fig)[i]);
   countMemoryEntries();
-  //for(int i = 0; fig[i]; i++)
-    //unregisterPointer((void**)&fig[i]);
+  unregisterPointer((void**)fig);
   countMemoryEntries();
 }
 
@@ -27,20 +27,19 @@ char** reverse(char** fig){
   int cols = 0;
   while(fig[0][++cols]);
 
-  char** newFig;
-  memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1));
+  //char** newFig;
+  //memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1));
 
-  //char** newFig = allocateMemory(rows, cols);
-/*
+  char** newFig = allocateMemory(rows, cols);
+  
   for(int i = 0; fig[i]; i++){
     for(int j = 0; fig[0][j]; j++)
       newFig[i][j] = fig[i][j];
     newFig[i][cols] = 0;
   }
   newFig[rows] = 0;
-*/
   fprintf(stderr,"%p\n", &newFig);
-  unlinkMemory(newFig);
+  unlinkMemory(&newFig);
   countMemoryEntries();
   return newFig;
 }
